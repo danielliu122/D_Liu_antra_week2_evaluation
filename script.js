b1dd150f73edc89ca1fde43a0367855ae9b99192
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2022-09-30 14:48:31 -0400

    2 files modified

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-console.log('Hello!');
-console.log('Hello!');
 // & ~~~~~~~~~~~~~~ get 20 random users info from 20 api call
 // get name,email,phone dob for each person
-var siteData, test;
+var siteData;
+var name, email, phone, dob;
+var users = {};
 for (var i = 0; i < 2; i++) {
   fetch('https://randomuser.me/api')
     .then((response) => response.json())
     .then((json) => {
-      //console.log(JSON.stringify(json));
-      //siteData = JSON.stringify(json);
-      siteData = json;
-      console.log(siteData.results[0]);
+      siteData = json.results[0];
+      //console.log(siteData.name);
+      Object.values(siteData.name).forEach((item) => {
+        console.log(item);
+        if (item.length > 0) {
+          name += item + ' ';
+        }
+      });
+      name = name.substring(9);
+      email = siteData.email;
+      phone = siteData.phone;
+      dob = siteData.dob.date;
+      console.log(i, name, email, phone, dob);
+      users.push(name, email, phone, dob);
     });
 }
