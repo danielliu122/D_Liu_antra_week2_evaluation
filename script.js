184df95c33d4753c3a498c9536835a521abd4a06
danielliu122 <48341240+danielliu122@users.noreply.github.com>
2022-09-30 15:30:10 -0400

    1 file modified

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 // get name,email,phone dob for each person
 var siteData;
 var username, email, phone, dob;
-var users,
-  temp = [];
 for (var i = 0; i < 2; i++) {
   fetch('https://randomuser.me/api')
     .then((response) => response.json())
@@ -16,16 +14,13 @@ for (var i = 0; i < 2; i++) {
         siteData.name.first +
         ' ' +
         siteData.name.last;
-      console.log(username);
+      //console.log(username);
 
       email = siteData.email;
       phone = siteData.phone;
       dob = siteData.dob.date.substring(0, 10);
-      console.log(i, username, email, phone, dob);
-      temp = (username, email, phone, dob);
-      users.push(temp);
-      console.log(users);
-      // username = '';
+
+      console.log(siteData.picture.large);
+      console.log(siteData.picture.large, username, email, phone, dob);
     });
 }
-console.log(users);
